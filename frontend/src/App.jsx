de0fead6cb118a10c0da1408be810a099b274984
shanilk2004 <shanilmohamedk@gmail.com>
2025-02-07 11:14:24 +0530

    4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-
-
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/Login" />} />
+        <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Signup" element={<Signup />} />
       </Routes>
@@ -16,5 +15,3 @@ function App() {
 }
 
 export default App;
-
-
